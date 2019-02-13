1ef9fa6c7c21e67bd966b92ac3f597d9946e3d56
malooney <matthew.looney@icloud.com>
2019-02-13 17:04:33 -0600

    20190213

--- R/S5.aggregate_allBrands_2010.R
+++ R/S5.aggregate_allBrands_2010.R
@@ -1,12 +1,4 @@
 
-#' Add together two numbers.
-#'
-#' @param x A number.
-#' @param y A number.
-#' @return The sum of \code{x} and \code{y}.
-#' @examples
-#' add(1, 1)
-#' add(10, 1)
 #' @export
 
 S5.aggregate_allBrands_2010 <- function( Market = 'CHICAGO',
